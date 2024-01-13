960758974adb484577e09f2c5392567f59b6a865
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2024-01-13 10:38:40 +0000

    database model

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Voluntario
+from .models import db, User, Voluntario, Ongs, Campaign
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,6 +13,10 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Voluntario, db.session))
+    admin.add_view(ModelView(Ongs, db.session))
+    admin.add_view(ModelView(Campaign, db.session))
+
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -24,9 +24,11 @@ class Voluntario(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     ciudad = db.Column(db.String(80), unique=False, nullable=False)
-    lat = db.Column(db.Integer, unique=True, nullable=False)
-    lng = db.Column(db.Integer, unique=True, nullable=False)
-    nth_campaign = db.Column(db.Integer, unique=True, nullable=False)
+    lat = db.Column(db.Float, unique=True, nullable=False)
+    lng = db.Column(db.Float, unique=True, nullable=False)
+    #campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=False)
+    #campaigns = db.relationship('Campaign', backref='voluntario', lazy=True)
+
 
 
     def __repr__(self):
@@ -38,5 +40,53 @@ class Voluntario(db.Model):
             "nombre": self.nombre,
             "ciudad": self.ciudad,
             "email": self.email,
+            #"campaigns": [campaign.nombre for campaign in self.campaigns],
             # do not serialize the password, its a security breach
+        }
+    
+class Ongs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), unique=False, nullable=False)
+    ciudad = db.Column(db.String(80), unique=False, nullable=False)
+    nif = db.Column(db.String(80), unique=False, nullable=False)
+    actividad = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    aprobado = db.Column(db.Boolean(), unique=False, nullable=False)
+    lat = db.Column(db.Float, unique=True, nullable=False)
+    lng = db.Column(db.Float, unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Ongs {self.email}>'
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "ciudad": self.ciudad,
+            "actividad": self.actividad,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class Campaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_inicio = db.Column(db.Date, nullable=False)
+    fecha_finalizacion = db.Column(db.Date, nullable=False)
+    nombre = db.Column(db.String(80), nullable=False)
+    ong_id = db.Column(db.Integer, db.ForeignKey('ongs.id'), nullable=False)
+    
+    ong = db.relationship('Ongs', backref=db.backref('campaigns', lazy=True))
+
+    def __repr__(self):
+        return f'<Campaign {self.nombre}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fecha_inicio": str(self.fecha_inicio),
+            "fecha_finalizacion": str(self.fecha_finalizacion),
+            "nombre": self.nombre,
+            "ong_id": self.ong_id,
         }
