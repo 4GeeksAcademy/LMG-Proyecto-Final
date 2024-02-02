95f805793215a80b8f8839d8c8475193ab0b1524
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2024-02-02 01:11:01 +0000

    added sections ans styles

--- src/front/js/component/sectionHeroBanner.js
+++ src/front/js/component/sectionHeroBanner.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const SectionHeroBanner = () => {
   return (
     <div className="hero-banner text-center ">
         <h1 className="fw-bold">Let me Give</h1>
-        <p className="lead mb-4">Lorem ipsum dolor dolor sit amet lorem ipsum dolor lorem ipsum dolor sit lorem </p>
+        <p className="lead mb-4">Conectamos corazones solidarios creando impacto real en el mundo</p>
         <div className="col-lg-6 mx-auto mt-5">
         <div className="md-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-                <button type="button" className="btn btn-secondary btn-lg px-4">Secondary</button>
+                <a href="/addOng" type="button" className="btn btn-secondary btn-lg px-4">Registra tu ONG</a>
+                <a href="/voluntarioSignup" type="button" className="btn btn-primary btn-lg px-4 gap-3">Hazte voluntario</a>
         </div>
         </div>
     </div>
