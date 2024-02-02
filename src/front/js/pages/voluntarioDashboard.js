47ed95c2b9e79f0459e1f5576797a1886726ea11
Aguilar234g <96350616+Aguilar234g@users.noreply.github.com>
2024-02-02 01:27:37 +0000

    logica completa

--- src/front/js/pages/voluntarioDashboard.js
+++ src/front/js/pages/voluntarioDashboard.js
@@ -1,15 +1,11 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
-import corazon from "../../img/corazon.gif";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 
 export const VoluntarioDashboard = () => {
     const { store, actions } = useContext(Context);
     const navigate = useNavigate();
     const [favorites, setFavorites] = useState([]);
-    const [showDonationModal, setShowDonationModal] = useState(false);
-  const [donatedCampaignInfo, setDonatedCampaignInfo] = useState(null);
 
     useEffect(() => {
         const id = localStorage.getItem("id");
@@ -18,88 +14,66 @@ export const VoluntarioDashboard = () => {
         setFavorites(favoritesFromStorage);
     }, []);
 
-        
-
     return (
         <div className="container w-75 page-container mt-3">
             {store.auth_voluntario === true ?
                 <>
                     <div key={store.voluntario.id} className="p-4 mb-3">
-                    <h1>Hola de nuevo, {store.voluntario.nombre}</h1>
-                          
-                            <div>
-                                <p>
-                                <strong>Nombre: </strong> {store.voluntario.nombre}<br/>
-                                <strong>Email: </strong>{store.voluntario.email}<br/>
-                                <strong>Ciudad: </strong>{store.voluntario.ciudad}<br/>
-                                <strong>Dirección: </strong>{store.voluntario.direccion}<br/>
-                                </p>
-                            </div>
-
-                            <>
-                                <button type="button" className="me-2 btn btn-action btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                                    Eliminar
-                                </button>
-
-                                <button type="button" className="btn btn-action btn-primary" onClick={() => navigate(`/editVoluntario/${store.voluntario.id}`)} >
-                                    Editar
-                                </button>
-                            </>
-
-                            <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                                <div className="modal-dialog">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <h1 className="modal-title fs-5" id="deleteModalLabel">¿Estás seguro?</h1>
-                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div className="modal-body">
-                                            Estás a punto de borrar este perfil
-                                        </div>
-                                        <div className="modal-footer">
-                                            <button type="button" className="btn btn-action btn-secondary" data-bs-dismiss="modal">Mejor no</button>
-                                            <button className="btn btn-action btn-primary" onClick={() => actions.deleteVoluntario(store.voluntario.id)}>Eliminar</button>
-                                        </div>
+                        <h1>Hola de nuevo, {store.voluntario.nombre}</h1>
+                        <div>
+                            <p>
+                                <strong>Nombre: </strong> {store.voluntario.nombre}<br />
+                                <strong>Email: </strong>{store.voluntario.email}<br />
+                                <strong>Ciudad: </strong>{store.voluntario.ciudad}<br />
+                                <strong>Dirección: </strong>{store.voluntario.direccion}<br />
+                            </p>
+                        </div>
+                        <>
+                            <button type="button" className="me-2 btn btn-action btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                Eliminar
+                            </button>
+                            <button type="button" className="btn btn-action btn-primary" onClick={() => navigate(`/editVoluntario/${store.voluntario.id}`)}>
+                                Editar
+                            </button>
+                        </>
+                        <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h1 className="modal-title fs-5" id="deleteModalLabel">¿Estás seguro?</h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        Estás a punto de borrar este perfil
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-action btn-secondary" data-bs-dismiss="modal">Mejor no</button>
+                                        <button className="btn btn-action btn-primary" onClick={() => actions.deleteVoluntario(store.voluntario.id)}>Eliminar</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                
+                    </div>
+
                     <div className="container ">
-                    <h3>Has colaborado en estas campañas</h3>
-                    <div className="row">
-                        {favorites.map((favorite, index) => (
-                            <div key={index} className="col-md-4">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{favorite.campaignName}</h5>
-                                        <p className="card-text">{favorite.ongName}</p>
+                        <h3>Has colaborado en estas campañas</h3>
+                        <div className="row">
+                            {favorites.map((favorite, index) => (
+                                <div key={index} className="col-md-4">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{favorite.campaignName}</h5>
+                                            <p className="card-text">{favorite.ongName}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                    </div>
-
-                    {/* <div className="container mt-3">
-                    <h3>Insignias</h3>
-                    
-                    {favorites.length === 0 && (
-                        <h1>hola</h1>
-                    )}
-                    {favorites.length >= 3 && (
-                        <h1>holaaaaaaaa</h1>
-                    )}
-                    {favorites.length >= 5 && (
-                        <h1>holaaaaaaaaaaaaaaaa</h1>
-                    )}
-                    </div> */}
 
                     <div className="container mb-3">
-                    <Link to="/">
-                    Volver a la Home
-                    </Link>
-                     </div>
+                        <Link to="/">Volver a la Home</Link>
+                    </div>
                     <br />
                     <br />
                 </>
